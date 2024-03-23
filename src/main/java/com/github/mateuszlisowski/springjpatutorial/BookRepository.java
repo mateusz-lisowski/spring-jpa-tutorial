2e1d7506ae8bda4cbb88d0cd527db190ee73425c
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-24 00:51:48 +0100

    Create custom query for obtaining books by given author

--- src/main/java/com/github/mateuszlisowski/springjpatutorial/BookRepository.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/BookRepository.java
@@ -2,7 +2,11 @@ package com.github.mateuszlisowski.springjpatutorial;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BookRepository extends JpaRepository<Book, UUID> {
+
+    List<Book> findAllByAuthor(String author);
+
 }
