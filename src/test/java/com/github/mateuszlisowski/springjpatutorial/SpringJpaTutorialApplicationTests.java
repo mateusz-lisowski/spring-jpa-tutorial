edbc61de87f90c3260db22f57177314c5f6773ee
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-23 13:36:42 +0100

    Initial commit

---
+++ src/test/java/com/github/mateuszlisowski/springjpatutorial/SpringJpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mateuszlisowski.springjpatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
