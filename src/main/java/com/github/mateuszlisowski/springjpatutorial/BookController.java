2942cdfd9f0dacda3717164cfd23b7c4a012f01b
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-24 00:35:24 +0100

    Add endpoint for getting all the books and book by given id

--- src/main/java/com/github/mateuszlisowski/springjpatutorial/BookController.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/BookController.java
@@ -1,8 +1,12 @@
 package com.github.mateuszlisowski.springjpatutorial;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class BookController {
@@ -18,6 +22,22 @@ public class BookController {
             @RequestBody Book book
     ) {
        return repository.save(book);
+    }
+
+    @GetMapping("/books")
+    public List<Book> readAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/books/{book-id}")
+    public Book readById(
+            @PathVariable("book-id") UUID uuid
+    ) {
+        Optional<Book> book = repository.findById(uuid);
+        if (book.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Book with id " + uuid + " not found");
+        }
+        return book.get();
     }
 
 }
