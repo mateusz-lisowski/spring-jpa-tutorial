7aa3fbdd55a7d41a1bb9e3b37b40fd7a9034f84d
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-25 00:03:00 +0100

    Add working delete endpoint

--- src/main/java/com/github/mateuszlisowski/springjpatutorial/book/BookController.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/book/BookController.java
@@ -1,4 +1,4 @@
-package com.github.mateuszlisowski.springjpatutorial;
+package com.github.mateuszlisowski.springjpatutorial.book;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
