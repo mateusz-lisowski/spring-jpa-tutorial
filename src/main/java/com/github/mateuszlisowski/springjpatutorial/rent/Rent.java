08203aad6671cacff86e227ba31e74203a15ca75
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-25 00:00:01 +0100

    Add foreign key for rent's member

--- src/main/java/com/github/mateuszlisowski/springjpatutorial/rent/Rent.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/rent/Rent.java
@@ -1,7 +1,12 @@
 package com.github.mateuszlisowski.springjpatutorial.rent;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.github.mateuszlisowski.springjpatutorial.book.Book;
+import com.github.mateuszlisowski.springjpatutorial.member.Member;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import org.hibernate.annotations.UuidGenerator;
 
@@ -14,8 +19,20 @@ public class Rent {
     @Id
     @UuidGenerator
     private String id;
+
     private Date rentStart;
+
     private Date rentEnd;
+
     private Date dueDate;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "member_id"
+    )
+    @JsonBackReference
+    private Member member;
+
+    private Book book;
 
 }
