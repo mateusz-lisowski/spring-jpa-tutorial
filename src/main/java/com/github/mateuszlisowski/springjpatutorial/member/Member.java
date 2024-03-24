0a1227bbd0b8c1f1664ad1a0a8506f866d92186f
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-24 23:59:34 +0100

    Add foreign key for member's rent

--- src/main/java/com/github/mateuszlisowski/springjpatutorial/member/Member.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/member/Member.java
@@ -1,9 +1,14 @@
 package com.github.mateuszlisowski.springjpatutorial.member;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.github.mateuszlisowski.springjpatutorial.rent.Rent;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import org.hibernate.annotations.UuidGenerator;
+
+import java.util.List;
 
 @Entity
 @Data
@@ -14,5 +19,11 @@ public class Member {
     private String id;
     private String firstName;
     private String lastName;
+    @OneToMany(
+            mappedBy = "member"
+    )
+    @JsonManagedReference
+    private List<Rent> rents;
+
 
 }
