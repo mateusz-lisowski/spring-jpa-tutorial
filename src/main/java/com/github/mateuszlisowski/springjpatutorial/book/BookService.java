13a1fc6e30bb53b20b0bc4b5cbcc2a84cef4ab03
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-28 20:07:44 +0100

    Add book serialization method

--- src/main/java/com/github/mateuszlisowski/springjpatutorial/book/BookService.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/book/BookService.java
@@ -1,4 +1,12 @@
 package com.github.mateuszlisowski.springjpatutorial.book;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
+
+    public BookSchema serializeBook(Book book) {
+        return new BookSchema(book.getTitle(), book.getAuthor());
+    }
+
 }
