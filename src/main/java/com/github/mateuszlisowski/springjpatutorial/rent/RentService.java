737bdf4f38927876d3d90645811cdc153b83786f
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-29 23:54:38 +0100

    Add partially functioning implementation of rent deserialization

--- src/main/java/com/github/mateuszlisowski/springjpatutorial/rent/RentService.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/rent/RentService.java
@@ -1,4 +1,26 @@
 package com.github.mateuszlisowski.springjpatutorial.rent;
 
 public class RentService {
+
+    public RentSchema serializeRent(Rent rent) {
+        return new RentSchema(
+                rent.getRentStart(),
+                rent.getRentEnd(),
+                rent.getDueDate(),
+                rent.getMember().getId(),
+                rent.getBook().getId()
+        );
+    }
+
+    public Rent deserializeRent(RentSchema schema) {
+        Rent rent = new Rent();
+        rent.setRentStart(schema.rentStart());
+        rent.setRentEnd(schema.rentEnd());
+        rent.setDueDate(schema.dueDate());
+
+        // rent.setMember();
+        // rent.setBook();
+
+        return rent;
+    }
 }
