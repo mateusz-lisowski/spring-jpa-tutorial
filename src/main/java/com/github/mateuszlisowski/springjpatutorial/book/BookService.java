c652d49969d72086c738ba701799086039c2a742
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-31 00:18:01 +0100

    Implement CRD functionality

--- src/main/java/com/github/mateuszlisowski/springjpatutorial/book/BookService.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/book/BookService.java
@@ -3,6 +3,7 @@ package com.github.mateuszlisowski.springjpatutorial.book;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -23,7 +24,24 @@ public class BookService {
        return book;
     }
 
+    public Book createBook(BookSchema schema) {
+        return repository.save(deserializeBook(schema));
+    }
+
     public Optional<Book> getBookById(UUID uuid) {
         return repository.findById(uuid);
     }
+
+    public List<Book> getAllBooks() {
+        return repository.findAll();
+    }
+
+    public void deleteBook(UUID uuid) {
+        Optional<Book> book = getBookById(uuid);
+        if (book.isEmpty()) {
+            throw new RuntimeException("Book with id: " + uuid + " not found");
+        }
+        repository.delete(book.get());
+    }
+
 }
