25bea201df25f05edf2b7d3aeba839d95e4f32b3
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-30 22:01:36 +0100

    Add function to fetch member by id

--- src/main/java/com/github/mateuszlisowski/springjpatutorial/member/MemberService.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/member/MemberService.java
@@ -1,6 +1,16 @@
 package com.github.mateuszlisowski.springjpatutorial.member;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
 public class MemberService {
+
+    private final MemberRepository repository;
 
     public MemberSchema serializeMember(Member member) {
         return new MemberSchema(member.getFirstName(), member.getLastName());
@@ -11,6 +21,10 @@ public class MemberService {
         member.setFirstName(schema.firstName());
         member.setLastName(schema.lastName());
         return member;
+    }
+
+    public Optional<Member> getMemberById(UUID uuid) {
+        return repository.findById(uuid.toString());
     }
 
 }
