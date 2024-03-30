a83337c7460e5ba3cb668d8f9f64076fd607124d
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-31 00:41:11 +0100

    Change all responses to book dto instead of book entities

--- src/main/java/com/github/mateuszlisowski/springjpatutorial/book/BookController.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/book/BookController.java
@@ -3,11 +3,10 @@ package com.github.mateuszlisowski.springjpatutorial.book;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
@@ -17,29 +16,33 @@ public class BookController {
 
     @PostMapping("/books")
     @ResponseStatus(HttpStatus.CREATED)
-    public Book create(
+    public BookSchema create(
             @RequestBody BookSchema bookSchema
     ) {
-       return service.createBook(bookSchema);
+        Book result = service.createBook(bookSchema);
+        return service.serializeBook(result);
     }
 
     @GetMapping("/books")
-    public List<Book> readAll() {
-        return service.getAllBooks();
+    public List<BookSchema> readAll() {
+        List<Book> result = service.getAllBooks();
+        return result.stream().map(service::serializeBook).collect(Collectors.toList());
     }
 
     @GetMapping("/books/{book-id}")
-    public Book readById(
+    public BookSchema readById(
             @PathVariable("book-id") UUID uuid
     ) {
-       return service.getBookById(uuid);
+        Book result = service.getBookById(uuid);
+        return service.serializeBook(result);
     }
 
     @GetMapping("/books/search/{author}")
-    public List<Book> readByAuthor(
+    public List<BookSchema> readByAuthor(
             @PathVariable String author
     ) {
-        return service.getAllBooksByAuthor(author);
+        List<Book> result = service.getAllBooksByAuthor(author);
+        return result.stream().map(service::serializeBook).collect(Collectors.toList());
     }
 
     @DeleteMapping("/books/{book-id}")
