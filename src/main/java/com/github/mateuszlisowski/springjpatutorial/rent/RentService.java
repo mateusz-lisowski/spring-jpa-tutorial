3a080dc938e93a2c5f95ba59d72768c1d3761a75
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-30 22:01:56 +0100

    Implement rent deserialization

--- src/main/java/com/github/mateuszlisowski/springjpatutorial/rent/RentService.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/rent/RentService.java
@@ -1,6 +1,20 @@
 package com.github.mateuszlisowski.springjpatutorial.rent;
 
+import com.github.mateuszlisowski.springjpatutorial.book.Book;
+import com.github.mateuszlisowski.springjpatutorial.book.BookService;
+import com.github.mateuszlisowski.springjpatutorial.member.Member;
+import com.github.mateuszlisowski.springjpatutorial.member.MemberService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
 public class RentService {
+
+    private final MemberService memberService;
+    private final BookService bookService;
 
     public RentSchema serializeRent(Rent rent) {
         return new RentSchema(
@@ -18,8 +32,18 @@ public class RentService {
         rent.setRentEnd(schema.rentEnd());
         rent.setDueDate(schema.dueDate());
 
-        // rent.setMember();
-        // rent.setBook();
+        Optional<Member> member = memberService.getMemberById(rent.getMember().getId());
+        if (member.isEmpty()) {
+            throw new RuntimeException("Error while deserializing rent");
+        }
+
+        Optional<Book> book = bookService.getBookById(rent.getBook().getId());
+        if (book.isEmpty()) {
+            throw new RuntimeException("Error while deserializing rent");
+        }
+
+        rent.setMember(member.get());
+        rent.setBook(book.get());
 
         return rent;
     }
