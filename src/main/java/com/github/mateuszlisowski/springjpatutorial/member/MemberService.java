6bbac9aab5c76f7549483816ce1666bbea872ed8
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-04-03 14:38:41 +0200

    Implement all CRS service utils

--- src/main/java/com/github/mateuszlisowski/springjpatutorial/member/MemberService.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/member/MemberService.java
@@ -1,8 +1,11 @@
 package com.github.mateuszlisowski.springjpatutorial.member;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -23,8 +26,25 @@ public class MemberService {
         return member;
     }
 
-    public Optional<Member> getMemberById(UUID uuid) {
-        return repository.findById(uuid.toString());
+    public Member createMember(MemberSchema schema) {
+        return repository.save(deserializeMember(schema));
+    }
+
+    public Member getMemberById(UUID uuid) {
+        Optional<Member> member = repository.findById(uuid);
+        if (member.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Member with id: " + uuid + " not found");
+        }
+        return member.get();
+    }
+
+    public List<Member> getAllMembers() {
+        return repository.findAll();
+    }
+
+    public void deleteMember(UUID uuid) {
+        Member member = getMemberById(uuid);
+        repository.delete(member);
     }
 
 }
