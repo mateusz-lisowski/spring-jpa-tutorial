097d17f67e27cde9cc5f9e4b78747dae94c59f29
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-04-03 14:28:58 +0200

    Changed all string ids to uuid ids

--- src/main/java/com/github/mateuszlisowski/springjpatutorial/member/Member.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/member/Member.java
@@ -3,10 +3,10 @@ package com.github.mateuszlisowski.springjpatutorial.member;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.github.mateuszlisowski.springjpatutorial.rent.Rent;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
-import org.hibernate.annotations.UuidGenerator;
 
 import java.util.List;
 import java.util.UUID;
@@ -16,7 +16,7 @@ import java.util.UUID;
 public class Member {
 
     @Id
-    @UuidGenerator
+    @GeneratedValue
     private UUID id;
     private String firstName;
     private String lastName;
--- src/main/java/com/github/mateuszlisowski/springjpatutorial/rent/Rent.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/rent/Rent.java
@@ -3,22 +3,19 @@ package com.github.mateuszlisowski.springjpatutorial.rent;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.github.mateuszlisowski.springjpatutorial.book.Book;
 import com.github.mateuszlisowski.springjpatutorial.member.Member;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Data;
-import org.hibernate.annotations.UuidGenerator;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Entity
 @Data
 public class Rent {
 
     @Id
-    @UuidGenerator
-    private String id;
+    @GeneratedValue
+    private UUID id;
     private Date rentStart;
     private Date rentEnd;
     private Date dueDate;
