4e81685fef2216daf177627c392e4a9f81b919e0
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-04-04 01:07:08 +0200

    Rewrite member response schema to contain member uuid

--- src/main/java/com/github/mateuszlisowski/springjpatutorial/member/MemberResponse.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/member/MemberResponse.java
@@ -1,6 +1,9 @@
 package com.github.mateuszlisowski.springjpatutorial.member;
 
-public record MemberSchema(
+import java.util.UUID;
+
+public record MemberResponse(
+        UUID id,
         String firstName,
         String lastName
 ) {
