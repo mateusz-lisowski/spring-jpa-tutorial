e3d833f89bfecba08bc629afd97708b8a8456df2
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-10-28 00:03:02 +0100

    Change book create schema to contain user's provided id

--- src/main/java/com/github/mateuszlisowski/springjpatutorial/book/schemas/BookCreate.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/book/schemas/BookCreate.java
@@ -1,6 +1,9 @@
 package com.github.mateuszlisowski.springjpatutorial.book.schemas;
 
+import java.util.UUID;
+
 public record BookCreate(
+        UUID id,
         String title,
         String author
 ) {
