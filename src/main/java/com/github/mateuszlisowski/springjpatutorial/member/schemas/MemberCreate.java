558afaa96a174dd9a24944207176d6d2c66297c6
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-10-28 00:03:12 +0100

    Change member create schema to contain user's provided id

--- src/main/java/com/github/mateuszlisowski/springjpatutorial/member/schemas/MemberCreate.java
+++ src/main/java/com/github/mateuszlisowski/springjpatutorial/member/schemas/MemberCreate.java
@@ -1,6 +1,9 @@
 package com.github.mateuszlisowski.springjpatutorial.member.schemas;
 
+import java.util.UUID;
+
 public record MemberCreate(
+        UUID id,
         String firstName,
         String lastName
 ) {
